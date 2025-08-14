9314c4bbe174e6f1cd6caca51aa273f9b0f95e75
Your Name <your@email.com>
2025-08-14 23:59:41 +0000

    Done

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/E-commerce-',
-    assetPrefix: '/E-commerce-',
     images: {
         remotePatterns: [
             {
